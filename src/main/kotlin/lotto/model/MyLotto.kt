1fb3f19e92337f9113f2db698de770f2fb496936
joon0447 <joon0447@naver.com>
2025-10-29 18:02:50 +0900

    feat(mylotto) : 로또 번호 오름차순 정렬 추가

--- src/main/kotlin/lotto/model/MyLotto.kt
+++ src/main/kotlin/lotto/model/MyLotto.kt
@@ -18,7 +18,7 @@ class MyLotto {
         val lottoCount = inputAmount / LOTTO_PRICE
         repeat(lottoCount) {
             val lottoNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT)
-            myLottos.add(lottoNumbers)
+            myLottos.add(lottoNumbers.sorted())
         }
     }
 
