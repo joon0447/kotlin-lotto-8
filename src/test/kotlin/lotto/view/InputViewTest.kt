df73b1e2202a8a416419b220f2fa1013171e0c2d
joon0447 <joon0447@naver.com>
2025-10-29 12:06:08 +0900

    test(inputview) : inputprice 테스트 코드 생성

---
+++ src/test/kotlin/lotto/view/InputViewTest.kt
@@ -0,0 +1,19 @@
+package lotto.view
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+
+class InputViewTest {
+
+    @Test
+    fun `inputPrice 값 입력 확인하기`() {
+        val fakeInput = "1000"
+        val inputStream = ByteArrayInputStream(fakeInput.toByteArray())
+        System.setIn(inputStream)
+
+        val result = InputView.inputPrice()
+
+        assertEquals("1000", result)
+    }
+}
