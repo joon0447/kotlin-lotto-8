2949f8c5f75ee173e619841251c7a49c622f26ab
joon0447 <joon0447@naver.com>
2025-10-29 14:38:21 +0900

    feat(model, controller) : 구입금액 검증 추가

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,11 +1,12 @@
 package lotto.controller
 
+import lotto.model.InputValidator
 import lotto.view.InputView
 
 class LottoController {
 
     fun run() {
         val purchaseAmount = InputView.inputPurchaseAmount()
-
+        InputValidator.validateInputPrice(purchaseAmount)
     }
 }
---
+++ src/main/kotlin/lotto/model/InputValidator.kt
@@ -0,0 +1,13 @@
+package lotto.model
+
+import lotto.message.ErrorMessage
+
+object InputValidator {
+
+    fun validateInputPrice(purchase: String) {
+        val inputPurchase = purchase.toIntOrNull()
+        require(inputPurchase != null) { ErrorMessage.AMOUNT_IS_NOT_NUMBER.text }
+        require(inputPurchase > 1000) { ErrorMessage.AMOUNT_IS_LESS_THAN_ONE_THOUSAND.text }
+        require(inputPurchase % 1000 == 0) { ErrorMessage.AMOUNT_IS_NOT_DIVISIBLE_BY_THOUSAND.text }
+    }
+}
