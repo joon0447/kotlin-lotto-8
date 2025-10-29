5d1b830c73efd5d608bdd0e2eedf659000b66c2a
joon0447 <joon0447@naver.com>
2025-10-29 14:33:12 +0900

    test(inputvalidator) : 테스트 코드 수정

--- src/test/kotlin/lotto/model/InputValidatorTest.kt
+++ src/test/kotlin/lotto/model/InputValidatorTest.kt
@@ -10,30 +10,30 @@ class InputValidatorTest {
 
     @Test
     fun `입력된 구입 금액이 정상입력일 때`() {
-        assertDoesNotThrow { InputValidator.validateInputPrice(2000) }
+        assertDoesNotThrow { InputValidator.validateInputPrice("2000") }
     }
 
     @Test
     fun `입력된 구입 금액이 1000원으로 나누어 떨어지지 않을 때`() {
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            InputValidator.validateInputPrice(150)
+            InputValidator.validateInputPrice("1500")
         }
-        assertEquals(ErrorMessage.AMOUNT_IS_NOT_DIVISIBLE_BY_THOUSAND, exception.message)
+        assertEquals(ErrorMessage.AMOUNT_IS_NOT_DIVISIBLE_BY_THOUSAND.text, exception.message)
     }
 
     @Test
-    fun `입력된 구입 금액이 0보다 작을 때`() {
+    fun `입력된 구입 금액이 1000원보다 작을 때`() {
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            InputValidator.validateInputPrice(-1500)
+            InputValidator.validateInputPrice("500")
         }
-        assertEquals(ErrorMessage.AMOUNT_IS_MINUS, exception.message)
+        assertEquals(ErrorMessage.AMOUNT_IS_LESS_THAN_ONE_THOUSAND.text, exception.message)
     }
 
     @Test
     fun `입력된 구입 금액이 숫자로 변환이 안될 때`() {
         val exception = assertThrows(IllegalArgumentException::class.java) {
-            InputValidator.validateInputPrice(-1500)
+            InputValidator.validateInputPrice("1205d")
         }
-        assertEquals(ErrorMessage.AMOUNT_IS_NOT_NUMBER, exception.message)
+        assertEquals(ErrorMessage.AMOUNT_IS_NOT_NUMBER.text, exception.message)
     }
 }
