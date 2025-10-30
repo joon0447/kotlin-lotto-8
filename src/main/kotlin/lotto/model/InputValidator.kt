6975fd46279a73029fc3204ff02e1caef12c3d83
joon0447 <joon0447@naver.com>
2025-10-30 14:24:18 +0900

    feat(inputvalidator) : 당첨 번호 입력값 검증 함수 추가

--- src/main/kotlin/lotto/model/InputValidator.kt
+++ src/main/kotlin/lotto/model/InputValidator.kt
@@ -10,4 +10,14 @@ object InputValidator {
         require(inputPurchase > 1000) { ErrorMessage.INVALID_AMOUNT_RANGE.formattedText() }
         require(inputPurchase % 1000 == 0) { ErrorMessage.INVALID_AMOUNT_UNIT.formattedText() }
     }
+
+    fun validateInputWinningNumber(winningNumbers: List<String>) {
+        val translateWinningNumbers = winningNumbers.map { it.toIntOrNull() }
+
+        require(translateWinningNumbers.size == 6) { ErrorMessage.INVALID_WINNING_NUMBERS_COUNT.formattedText() }
+        translateWinningNumbers.forEach { number ->
+            require(number != null) { ErrorMessage.INVALID_WINNING_NUMBERS_FORMAT.formattedText() }
+            require(number in 1..45) { ErrorMessage.INVALID_WINNING_NUMBERS_RANGE.formattedText() }
+        }
+    }
 }
