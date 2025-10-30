801c113f86acbfee0272ea164a8490f5e4ea17e3
joon0447 <joon0447@naver.com>
2025-10-30 15:44:42 +0900

    feat(errormessage) : 당첨 번호 중복 에러 메시지 추가

--- src/main/kotlin/lotto/message/ErrorMessage.kt
+++ src/main/kotlin/lotto/message/ErrorMessage.kt
@@ -9,6 +9,7 @@ enum class ErrorMessage(val text: String) {
     INVALID_WINNING_NUMBERS_COUNT("당첨 번호의 개수는 6개이어야 합니다."),
     INVALID_WINNING_NUMBERS_FORMAT("모든 당첨 번호는 숫자이어야 합니다."),
     INVALID_WINNING_NUMBERS_RANGE("모든 당첨 번호는 1~45 사이의 숫자이여야 합니다."),
+    INVALID_WINNING_NUMBERS_DUPLICATE("입력된 당첨 번호 중 중복된 값이 존재합니다."),
 
     INVALID_BONUS_NUMBER_FORMAT("보너스 번호는 숫자이어야 합니다."),
     INVALID_BONUS_NUMBER_RANGE("보너스 번호는 1~45 사이의 숫자이여야 합니다.");
