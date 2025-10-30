db03cd979c0988a5dc8bfc54fa70a598e0619b06
joon0447 <joon0447@naver.com>
2025-10-30 13:41:36 +0900

    feat(inputvalidator, errormessage) : 접두사 출력 함수 추가

--- src/main/kotlin/lotto/model/InputValidator.kt
+++ src/main/kotlin/lotto/model/InputValidator.kt
@@ -6,8 +6,8 @@ object InputValidator {
 
     fun validateInputPrice(purchase: String) {
         val inputPurchase = purchase.toIntOrNull()
-        require(inputPurchase != null) { ErrorMessage.AMOUNT_IS_NOT_NUMBER.text }
-        require(inputPurchase > 1000) { ErrorMessage.AMOUNT_IS_LESS_THAN_ONE_THOUSAND.text }
-        require(inputPurchase % 1000 == 0) { ErrorMessage.AMOUNT_IS_NOT_DIVISIBLE_BY_THOUSAND.text }
+        require(inputPurchase != null) { ErrorMessage.AMOUNT_IS_NOT_NUMBER.formattedText() }
+        require(inputPurchase > 1000) { ErrorMessage.AMOUNT_IS_LESS_THAN_ONE_THOUSAND.formattedText() }
+        require(inputPurchase % 1000 == 0) { ErrorMessage.AMOUNT_IS_NOT_DIVISIBLE_BY_THOUSAND.formattedText() }
     }
 }
