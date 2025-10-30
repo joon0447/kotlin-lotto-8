f97e52a02dfb247178bd3eff2ada8acc8e68bc91
joon0447 <joon0447@naver.com>
2025-10-30 15:30:43 +0900

    refactor(inputparser, inputvalidator) : List<String> 에서 List<Int?> 로 수정

--- src/main/kotlin/lotto/model/InputParser.kt
+++ src/main/kotlin/lotto/model/InputParser.kt
@@ -2,9 +2,9 @@ package lotto.model
 
 object InputParser {
 
-    fun parseWinningNumbers(rawWinningNumbers: String): List<String> {
-        val winningNumbers = mutableListOf<String>()
-        winningNumbers.addAll(rawWinningNumbers.split(","))
+    fun parseWinningNumbers(rawWinningNumbers: String): List<Int?> {
+        val winningNumbers = mutableListOf<Int?>()
+        winningNumbers.addAll(rawWinningNumbers.split(",").map{ it.toIntOrNull()})
         return winningNumbers
     }
 }
--- src/main/kotlin/lotto/model/InputValidator.kt
+++ src/main/kotlin/lotto/model/InputValidator.kt
@@ -11,11 +11,9 @@ object InputValidator {
         require(inputPurchase % 1000 == 0) { ErrorMessage.INVALID_AMOUNT_UNIT.formattedText() }
     }
 
-    fun validateInputWinningNumber(winningNumbers: List<String>) {
-        val translateWinningNumbers = winningNumbers.map { it.toIntOrNull() }
-
-        require(translateWinningNumbers.size == 6) { ErrorMessage.INVALID_WINNING_NUMBERS_COUNT.formattedText() }
-        translateWinningNumbers.forEach { number ->
+    fun validateInputWinningNumber(winningNumbers: List<Int?>) {
+        require(winningNumbers.size == 6) { ErrorMessage.INVALID_WINNING_NUMBERS_COUNT.formattedText() }
+        winningNumbers.forEach { number ->
             require(number != null) { ErrorMessage.INVALID_WINNING_NUMBERS_FORMAT.formattedText() }
             require(number in 1..45) { ErrorMessage.INVALID_WINNING_NUMBERS_RANGE.formattedText() }
         }
