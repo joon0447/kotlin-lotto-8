d81e7262bedea4b3f37cb040bfeab51a925d66be
joon0447 <joon0447@naver.com>
2025-10-30 12:37:25 +0900

    feat(inputmessage) : 당첨 번호 입력 문구 추가

--- src/main/kotlin/lotto/message/InputMessage.kt
+++ src/main/kotlin/lotto/message/InputMessage.kt
@@ -1,5 +1,6 @@
 package lotto.message
 
 enum class InputMessage(val text: String) {
-    PURCHASE_AMOUNT("구입금액을 입력해 주세요.")
+    PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
+    WINNING_NUMBERS("당첨 번호를 입력해 주세요.")
 }
