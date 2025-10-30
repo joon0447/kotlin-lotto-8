f7dcf2a036e6f22c70c2a19030f57d55b3199dee
joon0447 <joon0447@naver.com>
2025-10-30 15:03:29 +0900

    feat(errormessage) : 보너스 번호 에러메시지 추가

--- src/main/kotlin/lotto/message/ErrorMessage.kt
+++ src/main/kotlin/lotto/message/ErrorMessage.kt
@@ -8,7 +8,10 @@ enum class ErrorMessage(val text: String) {
 
     INVALID_WINNING_NUMBERS_COUNT("당첨 번호의 개수는 6개이어야 합니다."),
     INVALID_WINNING_NUMBERS_FORMAT("모든 당첨 번호는 숫자이어야 합니다."),
-    INVALID_WINNING_NUMBERS_RANGE("모든 당첨 번호는 1~45 사이의 숫자이여야 합니다.");
+    INVALID_WINNING_NUMBERS_RANGE("모든 당첨 번호는 1~45 사이의 숫자이여야 합니다."),
+
+    INVALID_BONUS_NUMBER_FORMAT("보너스 번호는 숫자이어야 합니다."),
+    INVALID_BONUS_NUMBER_RANGE("보너스 번호는 1~45 사이의 숫자이여야 합니다.");
 
     fun formattedText(): String {
         return "[ERROR] $text"
