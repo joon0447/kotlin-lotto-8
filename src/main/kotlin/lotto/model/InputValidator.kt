dad7740d6ee566386a104d20925542402d854f08
joon0447 <joon0447@naver.com>
2025-11-01 12:42:22 +0900

    fix(inputvalidator) : 1000원 초과 -> 1000원 이상으로 조건 변경

--- src/main/kotlin/lotto/model/InputValidator.kt
+++ src/main/kotlin/lotto/model/InputValidator.kt
@@ -7,7 +7,7 @@ object InputValidator {
     fun validateInputPrice(purchase: String) {
         val inputPurchase = purchase.toIntOrNull()
         require(inputPurchase != null) { ErrorMessage.INVALID_AMOUND_FORMAT.formattedText() }
-        require(inputPurchase > 1000) { ErrorMessage.INVALID_AMOUNT_RANGE.formattedText() }
+        require(inputPurchase >= 1000) { ErrorMessage.INVALID_AMOUNT_RANGE.formattedText() }
         require(inputPurchase % 1000 == 0) { ErrorMessage.INVALID_AMOUNT_UNIT.formattedText() }
     }
 
