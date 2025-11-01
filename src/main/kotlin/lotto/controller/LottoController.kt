bbe67d091203ec131fc10159aa5c50208fad4078
joon0447 <joon0447@naver.com>
2025-11-01 13:44:31 +0900

    refactor(lottocontroller) : 함수 길이 간소화

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -11,25 +11,48 @@ import lotto.view.OutputView
 class LottoController {
 
     fun run() {
+        val purchaseAmount = readPurchaseAmount()
+        val myLotto = createMyLotto(purchaseAmount)
+        val lotto = createWinningLotto()
+        val bonusNumber = readBonusNumber(lotto.getWinningNumbers())
+        val winningResults = calculateWinningResult(lotto, myLotto, bonusNumber)
+        printResults(purchaseAmount, winningResults)
+    }
+
+    private fun printResults(purchaseAmount: String, winningResults: Map<String, Int>) {
+        OutputView.printWinningStatistics(winningResults)
+        val profit = ProfitCalculator.calculateProfit(purchaseAmount.toInt(), winningResults)
+        OutputView.printProfit(profit.toDouble())
+    }
+
+    private fun readPurchaseAmount(): String {
         val purchaseAmount = InputView.inputPurchaseAmount()
         InputValidator.validateInputPrice(purchaseAmount)
+        return purchaseAmount
+    }
 
+    private fun readBonusNumber(parseWinningNumbers: List<Int>): String {
+        val bonusNumber = InputView.inputBonusNumber()
+        InputValidator.validateInputBonusNumber(bonusNumber, parseWinningNumbers)
+        return bonusNumber
+    }
+
+
+    private fun createMyLotto(purchaseAmount: String): MyLotto {
         val myLotto = MyLotto()
         myLotto.createLottos(purchaseAmount)
         OutputView.printLottoCount(myLotto.getLottos().size)
         OutputView.printLottosNumber(myLotto.getLottos())
+        return myLotto
+    }
 
+    private fun createWinningLotto(): Lotto {
         val rawWinningNumbers = InputView.inputWinningNumbers()
         val parseWinningNumbers = InputParser.parseWinningNumbers(rawWinningNumbers)
-        val lotto = Lotto(parseWinningNumbers)
+        return Lotto(parseWinningNumbers)
+    }
 
-        val bonusNumber = InputView.inputBonusNumber()
-        InputValidator.validateInputBonusNumber(bonusNumber, parseWinningNumbers)
-
-        val winningCount = lotto.calculateWinningResults(myLotto.getLottos(), bonusNumber.toInt())
-        OutputView.printWinningStatistics(winningCount)
-
-        val profit = ProfitCalculator.calculateProfit(purchaseAmount.toInt(), winningCount)
-        OutputView.printProfit(profit.toDouble())
+    private fun calculateWinningResult(lotto: Lotto, myLotto: MyLotto, bonusNumber: String): Map<String, Int> {
+        return lotto.calculateWinningResults(myLotto.getLottos(), bonusNumber.toInt())
     }
 }
